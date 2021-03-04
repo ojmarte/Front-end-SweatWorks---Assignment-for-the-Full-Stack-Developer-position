d08b25ffde0ab4e8d4da81041eb7fafe1414a6f5
Osvaldo <osvaldo00marte@gmail.com>
2021-03-04 19:05:12 -0400

    layout.js was added to be responsive

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,13 @@
 import React from "react"
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"; 
 import Appbar from './appbar.js';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import './layout.css';
 
 const Layout = ({ children }) => {
+  const matches = useMediaQuery('(max-width:768px)'); //send to the main component	
+  const crs = (matches, styles1, styles2) => ((matches) ? styles2 : styles1); //crs: chooseRightStyles
+
   const st1 = {
 
   }
@@ -20,12 +24,13 @@ const Layout = ({ children }) => {
 	marginLeft: "55px",
 	marginRight: "55px",
   }
+  const st3resp = { ...st3, marginLeft: "0px", marginRight: "0px", marginTop: "0px" };	
 
 	return (
    		 <div style={st1}>
 			<Appbar />
 			<div style={st2}/>
-			<main style={st3}>{children}</main>
+			<main style={crs(matches, st3, st3resp)}>{children}</main>
 			<div style={{ width: "100%", paddingTop: "30px"}} />
    		 </div>
  	 );
