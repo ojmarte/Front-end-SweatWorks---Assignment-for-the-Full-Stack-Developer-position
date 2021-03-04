d5109accbe3d7bcc374d00419caf4b3c3f2f323b
Osvaldo <osvaldo00marte@gmail.com>
2021-03-04 14:39:33 -0400

    lastStats.js a components of the challenge

---
+++ src/components/lastStats.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import UserRank from './rankStats.js';
+import MainStats from './mainStats.js';
+import './layout.css';
+import maria from '../images/maria.jpg';
+
+const AllRankStats = () => {
+    const [hasClass, setHasClass] = useState(true)
+    const turnOffClass = () => setHasClass(hasClass => !hasClass);	
+
+    const st1 = {
+	margin: 0,
+	padding: 0,
+	cursor: "pointer",
+	transition: "all .5s ease-in-out"
+    }
+
+	return (
+		<div style={st1} onClick={turnOffClass} role="button" onKeyPress={0} tabIndex="0">
+			<UserRank rank={1} image={maria} name="Maria Dorotea" bib={1255} age={25} gender="F" time="14:35" score={45} />
+			<div style={{ margin: "0px", padding: "0px" }} className={(hasClass) ? 'slide-down' : 'slide-down height'}><MainStats /></div>
+		</div>
+	);
+}
+
+export default AllRankStats
