ad2b2f30a1fd96b398e478585b8203b97bafe38a
Osvaldo <osvaldo00marte@gmail.com>
2021-03-04 19:07:35 -0400

    it was adapted to be main component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
 import React from "react";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Layout from "../components/layout.js";
 import Stats from "../components/stats.js";
 import AllRankStats from '../components/lastStats.js';
 
 
 const IndexPage = () => {
+   const matches = useMediaQuery('(max-width:768px)'); 
+   const chooseRightStyles = (st1, st2) => ((matches, styles1, styles2) => ((matches) ? styles2 : styles1))(matches, st1, st2);
  
 	return (
   	<Layout> 
             <Stats>
-		<AllRankStats />	
-		<AllRankStats />	  
+		<AllRankStats cts={chooseRightStyles}/>	
+		<AllRankStats cts={chooseRightStyles}/>	  
 	    </Stats>
   	</Layout>
 	)
